f4a26e7e84187d89aafdbdd7bd72ff6be18139d1
賴品言 <cheiling0131@gmail.com>
2026-08-01 16:46:22 +0800

    Update youtube-iframe.d.ts

--- src/types/youtube-iframe.d.ts
+++ src/types/youtube-iframe.d.ts
@@ -1,33 +1,33 @@
-declare namespace YT {
-  interface PlayerEvent {
-    data: number;
-  }
-
-  interface PlayerOptions {
-    videoId: string;
-    playerVars?: Record<string, string | number>;
-    events?: {
-      onStateChange?: (event: PlayerEvent) => void | Promise<void>;
-    };
-  }
-
-  interface Player {
-    destroy(): void;
-  }
-
-  interface PlayerConstructor {
-    new (element: HTMLElement, options: PlayerOptions): Player;
-  }
-
-  interface YTGlobal {
-    Player: PlayerConstructor;
-    PlayerState: {
-      ENDED: number;
-    };
-  }
-}
-
 declare global {
+  namespace YT {
+    interface PlayerEvent {
+      data: number;
+    }
+
+    interface PlayerOptions {
+      videoId: string;
+      playerVars?: Record<string, string | number>;
+      events?: {
+        onStateChange?: (event: PlayerEvent) => void | Promise<void>;
+      };
+    }
+
+    interface Player {
+      destroy(): void;
+    }
+
+    interface PlayerConstructor {
+      new (element: HTMLElement, options: PlayerOptions): Player;
+    }
+
+    interface YTGlobal {
+      Player: PlayerConstructor;
+      PlayerState: {
+        ENDED: number;
+      };
+    }
+  }
+
   interface Window {
     YT: YT.YTGlobal;
     onYouTubeIframeAPIReady?: () => void;
